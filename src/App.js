7fa23c6769cfe7b587d24659dd6214535039672d
Julian <juliaximo@gmail.com>
2022-09-01 21:41:03 -0300

    Slideshow 1st version

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Navbar from './components/Navbar.jsx'
+import Navbar from './components/Navbar.jsx';
+import Slideshow from './components/Slideshow.jsx';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Slideshow list={["a", "b"]}/>
     </div>
   );
 }
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,24 +1,19 @@
 import logo from "../images/logo_title.png"
 import logoText from "../images/logo_text.png"
+import { useState, useEffect } from "react";
 import "../styles/navbar.css";
+import React from "react";
 
-export default function Slideshow() {
+export default function Slideshow(props) {
 
-  return (
-    <nav>
-        <div style={{marginRight: "auto"}}>
-            <img className="logo" src={logo} alt="Logo Mundial" />
-            <img className="logo-text" src={logoText} alt="Texto Logo" />
-        </div>
-        <span className="hamburger" id="hamburger">
-            <i className="material-symbols-outlined">menu</i>
-        </span>
-        <ul className="nav__links" id="nav__links">
-            <li><a href={"#funciones"}>Funciones</a></li>
-            <li><a href={"#quienes-somos"}>Quienes somos</a></li>
-            <li><a href={"#contacto"}>Contacto</a></li>
-            <li id="login-bt"><a href={"accounts/login.html"}><button className="login-bt" >Ingresar</button></a></li>
-            <li id="my-acc"><a href={"accounts/profile.php"}><button className="login-bt" >Mi Cuenta</button></a></li>
-        </ul>
-    </nav>  );
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+      const timer = setTimeout(
+        () => (setIndex(index==props.length-1? 0 : index+1)),
+        5000
+      );
+      return () => clearTimeout(timer);
+    });
+
+    return <div>{props.list[index]}</div>;
 }
