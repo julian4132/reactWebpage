c5ac678ced8c977584beea5a1986b94fb7a169a3
Julian <juliaximo@gmail.com>
2022-09-03 18:23:12 -0300

    Improved slideshow

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -6,18 +6,27 @@ import React from "react";
 
 export default function Slideshow(props) {
 
-    const fade = false;
+    const [fade, setFade] = useState(false);
     const [index, setIndex] = useState(0);
     useEffect(() => {
+      if(fade){
       const timer = setTimeout(
-        () => (setIndex((index===props.list.length-1)? 0 : index+1)),
-        5000
+        () => (setFade(false), setIndex((index===props.list.length-1)? 0 : index+1)),
+        250
       );
       return () => clearTimeout(timer);
+    }
+    else{
+      const timer = setTimeout(
+        () => (setFade(true)),
+        4750
+      );
+      return () => clearTimeout(timer);
+    }
     });
 
     return (
-        <div className={fade? "fadeOut img" : "img"}>
+        <div className={fade? "fadeOut img" : "fadeIn img"}>
           <img src={props.list[index]} style={{"width": "100%"}}/>
         </div>
     );
