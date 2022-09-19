4ea761f8bf842c3f922d0af4ef1d1179b5c5c36a
Julian <juliaximo@gmail.com>
2022-09-18 22:51:24 -0300

    Sliding text

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar.jsx';
 import Slideshow from './components/Slideshow.jsx';
 import AccessButton from './components/AccessButton.jsx';
+import FadeIn from './components/FadeIn.jsx'
 import './App.css';
 import Pedri from './images/pedri.jpg'
 import Messi from './images/messi.jpg'
@@ -12,12 +13,18 @@ import VanDijk from './images/van_dijk.jpg'
 import Paqueta from './images/paqueta.jpg'
 import News from './images/001-news.png'
 
+
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Slideshow list={[Pedri, Messi, Cristiano, Debruyne, Suarez, Lewandoski, VanDijk, Paqueta]}/>
-      {<AccessButton title="Noticias" text="aaa" buttonText="bbb" imgsrc={News}/>}
+      {/*<AccessButton title="Noticias" text="aaa" buttonText="bbb" imgsrc={News}/>*/}
+      <FadeIn>
+        <div>
+          <p>Holaa</p>
+        </div>
+      </FadeIn>
     </div>
   );
 }
