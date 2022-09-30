f1eedf2b638e6cfd8d13be519b1cfa6c493b3d2b
Julian <juliaximo@gmail.com>
2022-09-30 11:27:12 -0300

    Landing page first version

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import {FaInstagram} from 'react-icons/fa'
+import {FaTwitter} from 'react-icons/fa'
+import {FaPhone} from 'react-icons/fa'
 
 export default function Contact() {
   return (
     <footer className="contact" id="contacto">
         <ul>
-            <li><a href="https://youtu.be/dQw4w9WgXcQ" style={{color: "inherit"}}><i className="fa fa-instagram"></i> Instagram</a></li>
-            <li><a href="https://youtu.be/dQw4w9WgXcQ" style={{color: "inherit"}}><i className="fa fa-twitter"></i> Twitter</a></li>
-            <li><a href="https://youtu.be/dQw4w9WgXcQ" style={{color: "inherit"}}><i className="fa fa-phone"></i> +54 9 341 696-9578</a></li>
+            <li><a href="https://youtu.be/dQw4w9WgXcQ" style={{color: "inherit"}}><FaInstagram /> Instagram</a></li>
+            <li><a href="https://youtu.be/dQw4w9WgXcQ" style={{color: "inherit"}}><FaTwitter /> Twitter</a></li>
+            <li><a href="https://youtu.be/dQw4w9WgXcQ" style={{color: "inherit"}}><FaPhone /> +54 9 341 696-9578</a></li>
         </ul>
     </footer>
   )
