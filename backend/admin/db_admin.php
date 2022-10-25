95971ed8c951c40bc15ec6039956547a16d5736b
Julian <juliaximo@gmail.com>
2022-10-25 10:11:18 -0300

    Fixed CORS bugs

--- backend/admin/db_admin.php
+++ backend/admin/db_admin.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: https://www.agssoft.ar/NUEVE');
-header('Access-Control-Allow-Credentials: true');
+/*header('Access-Control-Allow-Origin: https://www.agssoft.ar/NUEVE');
+header('Access-Control-Allow-Credentials: true');*/
 session_start();
 include_once "../accounts/connect.php";
 //$_SESSION['user_name'] == 'admin@admin.com'
--- backend/admin/doBan.php
+++ backend/admin/doBan.php
@@ -1,6 +1,6 @@
 <?php
-header('Access-Control-Allow-Origin: https://www.agssoft.ar/NUEVE');
-header('Access-Control-Allow-Credentials: true');
+/*header('Access-Control-Allow-Origin: https://www.agssoft.ar/NUEVE');
+header('Access-Control-Allow-Credentials: true');*/
 include_once "../accounts/connect.php";
 session_start();
 if($_SESSION['user_name']!='admin@admin.com') die();
